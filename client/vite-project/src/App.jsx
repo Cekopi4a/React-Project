ec5fb9316708fda55537a3508b68e2a53cfca55d
Cekopi4a <cecko207@abv.bg>
2023-11-14 13:47:44 +0200

    Initial commit

---
+++ client/vite-project/src/App.jsx
@@ -0,0 +1,34 @@
+import {Routes,Route} from 'react-router-dom'
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Shop from './components/Shop'
+import Home from './components/Home'
+import Login from './components/Login'
+import Footer from './components/Footer'
+import Admin from './components/Admin'
+import Item from './components/Item'
+import UserItem from './components/UserItem'
+
+
+
+
+
+function App() {
+  return (
+    <div>
+<Navbar />
+    <Routes>
+       <Route path='/' element={<Home />} />
+       <Route path='/shop' element={<Shop />} />
+       <Route path='/login' element={<Login />} />
+       <Route path='/admin' element={<Admin />} />
+       <Route path='/item' element={<Item />} />
+       <Route path='/myItem' element={<UserItem />} />
+</Routes>
+<Footer />
+    </div>
+  )
+}
+
+
+export default App
