dce8c6ac282667fd393b937ccd7e9e981a36b6eb
Cekopi4a <cecko207@abv.bg>
2023-11-17 09:58:04 +0200

    AdminUser-OK

--- client/vite-project/src/components/Admin.jsx
+++ client/vite-project/src/components/Admin.jsx
@@ -1,6 +1,18 @@
 import style from './Admin.module.css'
+import * as userService from "../service/userService";
+import { useEffect,useState } from 'react';
+import AdminUser from './AdminUsers';
 
 const Admin = () => {
+const [users, setUsers] = useState([]);
+
+console.log(users);
+
+useEffect(() => {
+   userService.getAll()
+   .then(result => setUsers(result));
+},[]);
+
     return(
   <>
   <div className="container-xl">
@@ -23,9 +35,10 @@ const Admin = () => {
 						<th>
 							<span className="custom-checkbox">
 								<input type="checkbox" id="selectAll" />
-								<label for="selectAll"></label>
+								<label htmlFor="selectAll"></label>
 							</span>
 						</th>
+						<th>Picture</th>
 						<th>Name</th>
 						<th>Email</th>
 						<th>Address</th>
@@ -33,88 +46,16 @@ const Admin = () => {
 						<th>Actions</th>
 					</tr>
 				</thead>
-				<tbody>
-					<tr>
-						<td>
-							<span className="custom-checkbox">
-								<input type="checkbox" id="checkbox1" name="options[]" value="1" />
-								<label for="checkbox1"></label>
-							</span>
-						</td>
-						<td>Thomas Hardy</td>
-						<td>thomashardy@mail.com</td>
-						<td>89 Chiaroscuro Rd, Portland, USA</td>
-						<td>(171) 555-2222</td>
-						<td>
-							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span className="custom-checkbox">
-								<input type="checkbox" id="checkbox2" name="options[]" value="1" />
-								<label for="checkbox2"></label>
-							</span>
-						</td>
-						<td>Dominique Perrier</td>
-						<td>dominiqueperrier@mail.com</td>
-						<td>Obere Str. 57, Berlin, Germany</td>
-						<td>(313) 555-5735</td>
-						<td>
-							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span className="custom-checkbox">
-								<input type="checkbox" id="checkbox3" name="options[]" value="1" />
-								<label for="checkbox3"></label>
-							</span>
-						</td>
-						<td>Maria Anders</td>
-						<td>mariaanders@mail.com</td>
-						<td>25, rue Lauriston, Paris, France</td>
-						<td>(503) 555-9931</td>
-						<td>
-							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span className="custom-checkbox">
-								<input type="checkbox" id="checkbox4" name="options[]" value="1" />
-								<label for="checkbox4"></label>
-							</span>
-						</td>
-						<td>Fran Wilson</td>
-						<td>franwilson@mail.com</td>
-						<td>C/ Araquil, 67, Madrid, Spain</td>
-						<td>(204) 619-5731</td>
-						<td>
-							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>					
-					<tr>
-						<td>
-							<span className="custom-checkbox">
-								<input type="checkbox" id="checkbox5" name="options[]" value="1" />
-								<label for="checkbox5"></label>
-							</span>
-						</td>
-						<td>Martin Blank</td>
-						<td>martinblank@mail.com</td>
-						<td>Via Monte Bianco 34, Turin, Italy</td>
-						<td>(480) 631-2097</td>
-						<td>
-							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr> 
-				</tbody>
+				{users.map(user =>(
+				<AdminUser 
+				key={user._id}
+				firstName={user.firstName}
+				lastName={user.lastName}
+				email={user.email}
+				address={user.address}
+				phoneNumber={user.phoneNumber}
+				imageUrl={user.imageUrl}
+				/>))}
 			</table>
 			<div className="clearfix">
 				<div className="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
